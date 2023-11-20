5c7848263c0fa4e3522071f9c231c8ca649c4565
AndrianKusumaP <97347391+AndrianKusumaP@users.noreply.github.com>
2023-11-20 09:36:49 +0800

    Add files via upload

---
+++ admin/sql.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "localhost";
+    $user   = "root";
+    $password = "";
+    $database = "2101020018_nyomanadiandriankusumaputra";
+
+    $con    = mysqli_connect($server, $user, $password, $database);
+?>
